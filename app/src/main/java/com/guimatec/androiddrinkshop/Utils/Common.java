78421dfef87e71a8ec36c6eeffd659ed0e5eca88
JeanioGuimaraes <guimatec36@gmail.com>
2018-07-22 15:07:38 -0300

    Start commit!!!

---
+++ app/src/main/java/com/guimatec/androiddrinkshop/Utils/Common.java
@@ -0,0 +1,16 @@
+package com.guimatec.androiddrinkshop.Utils;
+
+import com.guimatec.androiddrinkshop.Retrofit.IDrinkShopAPI;
+import com.guimatec.androiddrinkshop.Retrofit.RetrofitClient;
+
+public class Common {
+    //No Emulador, localhost = 10.0.2.2
+
+    private static final String BASE_URL = "http://192.168.0.10/drinkshop/";
+
+    public static IDrinkShopAPI getAPI()
+    {
+        return RetrofitClient.getClient(BASE_URL).create(IDrinkShopAPI.class);
+
+    }
+}
